fe940514603ff1dac064dcb02b5b9d98a1272f79
Okechukwu Obi <okechukwuobicode@gmail.com>
2022-04-26 18:53:38 +0100

    SlitScreen Layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { SplitScreen } from './SplitScreen';
+
+
+const LeftHandComponent = () => {
+  return <h1 style={{background: 'green'}}>Left</h1>
+}
+
+const RightHandComponent = () => {
+  return <h1 style={{background: 'red'}}>Right</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen 
+      left={LeftHandComponent}
+      right={RightHandComponent}
+      leftWeight={1}
+      rightWeight={3}
+    />
   );
 }
 
